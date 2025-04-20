3cdba6ffb771211da8d7205ba63914e0956d8e48
trayouras <trayouras@gmail.com>
2025-04-20 16:45:31 -0700

    Style tweaks

--- src/app/page.js
+++ src/app/page.js
@@ -61,7 +61,7 @@ const styles = {
     textAlign: "center",
     maxWidth: "800px",
     margin: "30px",
-    lineHeight: "300px",
+    lineHeight: "30px",
   },
   header: {
     borderBottom: "10px solid #ddd",
