64c369cfb2a4cceecca11b0d075a6fcab2320ea4
trayouras <trayouras@gmail.com>
2025-04-20 17:04:48 -0700

    Style tweaks

--- src/app/page.js
+++ src/app/page.js
@@ -59,7 +59,7 @@ const styles = {
     fontFamily: "Arial, sans-serif",
     padding: "30px",
     textAlign: "right",
-    maxWidth: "80px",
+    maxWidth: "2000px",
     margin: "30px",
     //lineHeight: "30px",
   },
